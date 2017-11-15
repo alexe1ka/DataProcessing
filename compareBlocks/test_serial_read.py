7183f1e0b1d689dc055e451f17a426475c23f423
alexe1ka <leshka_09@bk.ru>
2017-11-15 16:03:46 +0300

    test serial test

--- compareBlocks/test_serial_read.py
+++ compareBlocks/test_serial_read.py
@@ -1,14 +1,22 @@
+import binascii
 import serial
-import time
 
-z1baudrate = 115200
-z1port = 'COM3'  # set the correct port before run it
+
+def write_to_file(something_data, filename):
+    writing_file = open(filename, 'w')
+    writing_file.write(str(something_data))
+    # for line in something_data:
+    #     writing_file.write(line)
+    writing_file.close()
+
+
+z1baudrate = 256000
+z1port = 'COM6'  # set the correct port before run it
 
 z1serial = serial.Serial(port=z1port, baudrate=z1baudrate)
-z1serial.timeout = 2  # set read timeout
+z1serial.timeout = 0  # set read timeout
 # print z1serial  # debug serial.
 print(z1serial.isOpen())  # True for opened
-
 
 packet = []
 count = 0
@@ -16,13 +24,18 @@ if z1serial.isOpen():
     while True:
         size = z1serial.inWaiting()  # сколько в порту есть - столько и читаем
         if size:
-            if count == 9:
+            data = z1serial.read(size)
+            data_decode = binascii.hexlify(data)  # преобразуем к нормальному виду
+            packet.append(data_decode)
+            print(data_decode)
+            count += 1
+            # time.sleep(1)
+            # if count > 8:
+            if count > 8:
                 break
-            else:
-                data = z1serial.read(size)
-                packet.append(data)
-                print(data)
-                count += 1
-        time.sleep(1)
+
 else:
     print('z1serial not open')
+
+z1serial.close()
+write_to_file(packet, "receive_data_from_bs")
